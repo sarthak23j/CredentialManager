1892648360bba3201989b8d5f3045ba8052b67ed
Sarthak <sarthak23j@gmail.com>
2025-12-21 18:08:54 +0530

    updated host on server.py

--- server.py
+++ server.py
@@ -95,4 +95,4 @@ def import_creds():
     return jsonify({"success": True, "count": count})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
